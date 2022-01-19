8f7d8ec46bba050c4527b34934a3e28a302c920f
khanhhoa20 <khanhhoa12t2@gmail.com>
2022-01-19 22:34:24 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Tweet from './Tweet';
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [data, setData] = useState([])
+  const fetchData = async () => {
+    try {
+      const response =
+        await axios.get('https://coursera-twitter-api.herokuapp.com/tweets')
+      setData(response.data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+
+    fetchData()
+    console.log('here');
+  }, [])
   return (
-    <h1>Hello World</h1>
+    <div
+      style={{
+        background: '#111111',
+        padding: '30px'
+      }}
+    >
+      {data.map(tweet =>
+        <Tweet
+          key={tweet.id}
+          {...tweet} />)}
+    </div>
   );
 }
 
